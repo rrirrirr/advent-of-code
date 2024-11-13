d02d3e1db440e1d5ddbb49ffa85893f01a9d9f6b
rrirrirr <drpihl@gmail.com>
2024-11-13 03:47:46 +0100

    fix part 2

--- 2021/ex/lib/day06.ex
+++ 2021/ex/lib/day06.ex
@@ -1,64 +1,54 @@
 defmodule Day06 do
   def part_1(file) do
-    fish = parse_input(file)
+    file
+    |> parse_input()
+    |> simulate_days(80)
+  end
 
-    1..80
+  def part_2(file) do
+    fish = parse_input(file)
+    spawn_counts = calculate_spawn_counts(256)
+
+    fish
+    |> Enum.map(&Enum.at(spawn_counts, 255 - &1))
+    |> Enum.sum()
+    |> Kernel.+(length(fish))
+  end
+
+  defp parse_input(file) do
+    file
+    |> File.read!()
+    |> String.split([",", "\n"], trim: true)
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  defp simulate_days(fish, days) do
+    1..days
     |> Enum.reduce(fish, fn _day, current_fish ->
       Enum.flat_map(current_fish, &process_fish/1)
     end)
     |> length()
   end
 
-  def part_2(file) do
-    fish =
-      parse_input(file)
-
-    fish_per_day =
-      1..20
-      |> Enum.reduce([], fn day, fish_list ->
-        fish_list ++ [count_fish(fish_list, day)]
-      end)
-
-    fish_per_day |> Enum.with_index()
-    # Enum.map(fish, &Enum.at(fish_per_day, 20 - &1)) |> Enum.sum()
-  end
-
-  defp parse_input(file) do
-    File.read!(file)
-    |> String.split([",", "\n"])
-    |> Enum.filter(&(&1 != "" and &1 != "\n"))
-    |> Enum.map(&String.to_integer/1)
-  end
-
   defp process_fish(0), do: [6, 8]
   defp process_fish(fish), do: [fish - 1]
 
-  defp count_fish(_, day) when day < 1, do: 0
+  defp calculate_spawn_counts(max_days) do
+    0..max_days
+    |> Enum.reduce([], &(&2 ++ [count_fish(&2, &1)]))
+  end
+
+  defp count_fish(_, day) when day < 0, do: 0
   defp count_fish(_, day) when day < 7, do: 1
 
-  defp count_fish(fish_list, day) when length(fish_list) >= day,
+  defp count_fish(fish_list, day) when length(fish_list) > day,
     do: Enum.at(fish_list, day)
 
-  # defp count_fish(fish_list, day) do
-  #   spawn_days = day..0//-7 |> Enum.to_list()
-  # IO.inspect("Day #{day}, spawn days: #{inspect(spawn_days)}")
-
-  # spawns_of_spawns =
-  #   for spawn_day <- spawn_days do
-  #     count_fish(fish_list, spawn_day - 9)
-  #   end
-
-  # IO.inspect("Spawns: #{inspect(spawns_of_spawns)}")
-  # IO.inspect("total: #{inspect(Enum.sum(spawns_of_spawns) + length(spawn_days))}")
-  #   Enum.sum(spawns_of_spawns) + 1
-  # end
-
   defp count_fish(fish_list, day) do
-    # Enum.sum(&count_fish(fish_list, &1 - 9))
-    spawn_days =
-      day..0//-7 |> Enum.to_list() |> Enum.sum()
-
-    IO.inspect("Spawn days: #{inspect(spawn_days)}")
-    spawn_days + 1
+    day..0//-7
+    |> Enum.to_list()
+    |> Enum.map(&count_fish(fish_list, &1 - 9))
+    |> Enum.map(&(&1 + 1))
+    |> Enum.sum()
   end
 end
